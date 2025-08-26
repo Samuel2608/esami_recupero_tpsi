3d36959724e580db31478dedc063784d71559efe
Samuel2608 <samuel.cassata@davincimilazzo.edu.it>
2025-08-26 12:18:10 +0200

    Add files via upload

---
+++ esami_recuprero_tpsi es1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#define DIM_MACRO(x,y) (x+y);
+int main (){
+    int x = 4;
+    int y =5;
+    int dimensione 1, dimensione 2;
+    dimensione 1= DIM_MACRO(x,y);
+    printf ("il vettore x ha dimensione %d\n", dimensione 1);
+
+}
---
+++ esami_recuprero_tpsi es2.c
@@ -0,0 +1,9 @@
+#include<stdio.h> 
+int main(int, int){
+int somma (int a ,int b ){
+int z;
+z = a+b;
+printf ("il risultato delle somme è %d\n" z);
+return z;
+}
+}
