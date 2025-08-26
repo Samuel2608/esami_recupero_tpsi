3cd13411d5cec77e2abd12a0bb04314224c407ac
Samuel2608 <samuel.cassata@davincimilazzo.edu.it>
2025-08-26 12:32:32 +0200

    Add files via upload

---
+++ esami_recuprero_tpsi es3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main (){
+    int vet[10=0,1,2,3,4,5,6,7,8,9];
+    int somma_vet(int vet[10]);
+    for (int i=0; i<10;i++){
+        somma = somma + [i];
+    }
+    printf("la somma di tutti gli elementi è %d\n" , vet);
+}
